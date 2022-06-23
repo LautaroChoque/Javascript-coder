96151cea356cfb9c89ba2f3afeff749c0241bd12
LautaroChoque <104146948+LautaroChoque@users.noreply.github.com>
2022-06-23 15:12:56 -0300

    empezando js

---
+++ script.js
@@ -0,0 +1,11 @@
+// string o texto
+var nombre = "Lautaro"; 
+var apellido = "Choque";
+// number
+var sueldo = 1000
+// boolean o logica
+var esEstudiante = true
+// null o vacio
+var esPropietario = null
+// undefined o no definido
+var esPadre = undefined
